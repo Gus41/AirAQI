0662e1a37c58c36c92c7c476e0b5aefe2aed3fae
Gustavo Damasio <gdoliveira2@ucs.br>
2024-09-25 15:29:33 -0300

    .

--- air/src/main.ts
+++ air/src/main.ts
@@ -1,54 +1,49 @@
-const apiKey = import.meta.env.VITE_AIRVISUAL_API_KEY
+const apiKey = import.meta.env.VITE_AIRVISUAL_API_KEY;
 const url = `https://api.airvisual.com/v2/nearest_city?key=${apiKey}`;
+const CACHE_DURATION = 3600 * 1000; // 1 hora em milissegundos
 
-function load(data : any){
+function load(data : any) {
   const city = document.querySelector('.city');
   const state = document.querySelector('.state');
   const country = document.querySelector('.country');
   const aqi = document.querySelector('.aqi');
-  const description = document.querySelector(".description")
-  const status = document.querySelector(".aqi-status")  
-  if(data){
-    if(city){
-      city.textContent = data.city
-    }
-    if(state){
-      state.textContent = "Estado: " + data.state
-    }
-    if(country){
-      country.textContent = "País: " + data.country
-    }
-    if(aqi){
-      aqi.textContent = data.aqi
-    }
-    if(description && status){
-      if(data.aqi <= 50){
-        description.textContent = comparable[50]
-        status.textContent = 'Satisfatório'
-      }else if(data.aqi <= 100 && data.aqi > 50){
-        description.textContent = comparable[100]
-        status.textContent = 'Moderado'
-      }else if(data.aqi <= 150 && data.aqi > 100){
-        description.textContent = comparable[150]
-        status.textContent = 'Não saudável para grupos sensíveis'
-      }else if(data.aqi <= 200 && data.aqi > 150){
-        description.textContent = comparable[200]
-        status.textContent = 'Extremamente prejudicial à saúde'
-      }else if(data.aqi <= 300 && data.aqi > 200){
-        description.textContent = comparable[300]
-        status.textContent = 'Muito Pessimo'
-      }else{
-        description.textContent = comparable[500]
-        status.textContent = 'Perigoso'
+  const description = document.querySelector(".description");
+  const status = document.querySelector(".aqi-status");
+  
+  if (data) {
+    if (city) city.textContent = data.city;
+    if (state) state.textContent = "Estado: " + data.state;
+    if (country) country.textContent = "País: " + data.country;
+    if (aqi) aqi.textContent = data.aqi;
+
+    if (description && status) {
+      if (data.aqi <= 50) {
+        description.textContent = comparable[50];
+        status.textContent = 'Satisfatório';
+      } else if (data.aqi <= 100 && data.aqi > 50) {
+        description.textContent = comparable[100];
+        status.textContent = 'Moderado';
+      } else if (data.aqi <= 150 && data.aqi > 100) {
+        description.textContent = comparable[150];
+        status.textContent = 'Não saudável para grupos sensíveis';
+      } else if (data.aqi <= 200 && data.aqi > 150) {
+        description.textContent = comparable[200];
+        status.textContent = 'Extremamente prejudicial à saúde';
+      } else if (data.aqi <= 300 && data.aqi > 200) {
+        description.textContent = comparable[300];
+        status.textContent = 'Muito Pessimo';
+      } else {
+        description.textContent = comparable[500];
+        status.textContent = 'Perigoso';
       }
     }
   }
 }
 
-function loadError(){
-  const description = document.querySelector(".description")
-  if(description){
-    description.textContent = 'Parece que ocorreu um erro'
+function loadError() {
+  const description = document.querySelector(".description");
+  if (description) {
+    description.textContent = 'Parece que ocorreu um erro';
   }
 }
 
@@ -59,40 +54,52 @@ const comparable = {
   200: 'Todos podem começar a sentir impactos na saúde; membros de grupos vulneráveis podem sofrer consequências mais graves. - Crianças, adultos ativos e pessoas com condições respiratórias, como asma, devem evitar atividades prolongadas ao ar livre; todos os demais, especialmente as crianças, devem restringir esforços prolongados fora de casa.',
   300: 'Avisos de saúde sobre situações de emergência. Toda a população tem maior chance de ser afetada. - Crianças, adultos ativos e pessoas com condições respiratórias, como asma, devem evitar todas as atividades ao ar livre; todos os demais, especialmente as crianças, devem limitar suas atividades externas.',
   500: 'Perigoso - Alerta de saúde: todos podem enfrentar efeitos mais graves à saúde.',
-}
-
+};
 
 async function hasCache() {
-  let data = null
   const cachedData = localStorage.getItem('airQuality');
-  if (cachedData) {
-    console.log('Has cache')
-    data = await JSON.parse(cachedData);
-    load(data)
-   
+  const cachedTime : number | any = localStorage.getItem('cacheTime');
+
+  if (cachedData && cachedTime) {
+    const now = new Date().getTime();
+    const cacheAge = now - cachedTime;
+
+    if (cacheAge < CACHE_DURATION) {
+      //console.log('Cache válido');
+      const data = await JSON.parse(cachedData);
+      load(data);
+      return;
+    } else {
+      //console.log('Cache expirado');
+      localStorage.removeItem('airQuality');
+      localStorage.removeItem('cacheTime');
+      getData();
+    }
   } else {
-    console.log('No cache')
-    getData()
+    console.log('Nenhum cache disponível');
+    getData();
   }
 }
-hasCache()
-
 
 async function getData() {
-  fetch(url).then(response => response.json()).then(data => {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const city = data.data.city;
+      const state = data.data.state;
+      const country = data.data.country;
+      const aqi = data.data.current.pollution.aqius;
 
-    const city = data.data.city;
-    const state = data.data.state;
-    const country = data.data.country;
-    const aqi = data.data.current.pollution.aqius; 
+      const now = new Date().getTime();
+      localStorage.setItem('airQuality', JSON.stringify({ city, state, country, aqi }));
+      localStorage.setItem('cacheTime', now.toString());
 
-    localStorage.setItem('airQuality', JSON.stringify({ city, state, country, aqi }));
-    load({ city, state, country, aqi })
-  }).catch(error => {
-    console.error('Erro ao buscar os dados:', error);
-    loadError()
-    return null;
-  });
+      load({ city, state, country, aqi });
+    })
+    .catch(error => {
+      console.error('Erro ao buscar os dados:', error);
+      loadError();
+    });
 }
 
-
+hasCache();
